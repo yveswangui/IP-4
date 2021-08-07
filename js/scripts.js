03ebb7496053eda833dab1137de84291f6967c87
yvonnewangui <yvonne.wamburu@student.moringaschool.com>
2021-08-07 17:00:58 +0300

    add click function to delivery button

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//business logic
+
+
+
+
+
+
+
+//user-interface logic
+$(document).ready(function(){
+        $('input[type = "radio"]#no-deliver').click(function(){
+            $(".location").hide();
+        });
+        $('input[type = "radio"]#yes-deliver').click(function(){
+            $(".location").show();
+        })
+});
