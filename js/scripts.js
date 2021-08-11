db0f43c1019f285b41e2264288c72e8a9bd7a6fc
yvonnewangui <yvonne.wamburu@student.moringaschool.com>
2021-08-11 11:19:43 +0300

    add click function to submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 //business logic
-
+function Customer(name){
+    this.customerName = name;
+}
 
 
 
@@ -15,26 +17,46 @@ $(document).ready(function(){
         $(".location").show();
     });
 })
+$(document).ready(function(){
+    $("form#order").submit(function(event){
+        event.preventDefault()
+        var inputtedName = $("input#new-customer-name").val();
+        var newCustomer = new Customer(inputtedName);
+        alert(newCustomer);
+    });
+});
 $ (document).ready(function(){
     $(".pizza-size").click(function(event){
         $(".pizza-toppings").click(function(event){
             $(".form-quantity").click(function(event){
-            var total = 0;
-            $(".pizza-size").each(function(){
-                total += parseInt($(this).val())
-            });
-                $(".pizza-toppings").each(function(){
-                    var toppingsTotal = total + parseInt($(this).val())
-                
-                
-                    $(".form-quantity").each(function(){
-                        var orderTotal = toppingsTotal * parseInt($(this).val())
-                        console.log(orderTotal);
-                    })
-                })
-                
-            
-                })
+                $(".towns").click(function(event){
+                    
+
+                var total = 0;
+                $(".pizza-size").each(function(){
+                    total += parseInt($(this).val())
+                });
+                    $(".pizza-toppings").each(function(){
+                        var toppingsTotal = total + parseInt($(this).val())
+                    
+                    
+                        $(".form-quantity").each(function(){
+                            var orderTotal = toppingsTotal * parseInt($(this).val())
+                            
+
+                            $(".towns").each(function(){
+                                var finalCharges = orderTotal + parseInt($(this).val())
+                                //console.log(finalCharges)
+                                 //alert("We got your order, your charges will be " + finalCharges)
+                                    $("#submit").click(function(){
+                                        alert("We got your order, total cost will be " + "Ksh." + finalCharges +" . Your pizza will be delivered to you in 20 minutes.")
+
+                                    })
+                                 });
+                                })
+                            });
+                        });
+                    });
+                });
             });
         });
-    });
