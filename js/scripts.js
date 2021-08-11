a8ac28453a78c73f11105b15278862fea235ec3d
yvonnewangui <yvonne.wamburu@student.moringaschool.com>
2021-08-11 21:07:25 +0300

    alert for select option values

--- js/scripts.js
+++ js/scripts.js
@@ -17,57 +17,58 @@ $(document).ready(function () {
         $(".location").show();
     });
 })
+
 $(document).ready(function () {
-    $("form#order").submit(function (event) {
-        event.preventDefault()
-        var inputtedName = $("input#new-customer-name").val();
-        var newCustomer = new Customer(inputtedName);
-        console.log(newCustomer);
+    $("#check-order").click(function(){
+    $("#p-size").change(function () {
+        alert($(this).find(':selected').data('size'));
     });
 });
-$(document).ready(function () {
-    $("#pizza-toppings").change(function() {
-        console.log($(this).find(':selected').data('topping'));
+    $("#pizza-toppings").change(function () {
+        alert($(this).find(':selected').data('topping'));
     });
-        $("#p-size").change(function(){
-            console.log($(this).find(':selected').data('size'));
+        $("#pizza-crust").change(function () {
+            alert($(this).find(':selected').data('crust'))
         });
-            $("#pizza-crust").change(function(){
-                console.log($(this).find(':selected').data('crust'));
-            })
     });
 
 
 
-//selection prices//
+
+//price selection//
 $(document).ready(function () {
+
     $(".pizza-size").change(function (event) {
-    var total = 0;
+        var total = 0;
         $(".pizza-size").each(function () {
             total += parseInt($(this).val())
-            
+
             $(".pizza-toppings").change(function (event) {
-            $(".pizza-toppings").each(function () {
+                $(".pizza-toppings").each(function () {
                     var toppingsTotal = total + parseInt($(this).val())
-                
-                    $(".pizza-crust").change(function(event){
-                    $(".pizza-crust").each(function(){
-                        var crustTotal = toppingsTotal + parseInt($(this).val())
-                
-                        $(".form-quantity").change(function (event) {
-                        $(".form-quantity").each(function () {
-                                var orderTotal = crustTotal * parseInt($(this).val())
 
-                            $(".towns").change(function (event) {
-                                var finalCharges = orderTotal + parseInt($(this).val())
-                                        //console.log(finalCharges)
-                                        //alert("We got your order, your charges will be " + finalCharges)
-                                $("#submit").click(function () {
-                                    alert("We got your order, total cost will be " + "Ksh." + finalCharges + " . Your pizza will be delivered to you in 20 minutes.")
+                    $(".pizza-crust").change(function (event) {
+                        $(".pizza-crust").each(function () {
+                            var crustTotal = toppingsTotal + parseInt($(this).val())
 
+                            $(".form-quantity").change(function (event) {
+                                $(".form-quantity").each(function () {
+                                    var orderTotal = crustTotal * parseInt($(this).val())
+
+                                    //$('input[type = "radio"]#yes-deliver').click(function () {               
+                                    $(".towns").change(function (event) {
+                                        $(".towns").each(function () {
+                                            var finalCharges = orderTotal + parseInt($(this).val())
+                                            //console.log(finalCharges)
+                                            //alert("We got your order, your charges will be " + finalCharges)
+
+                                            $("#submit").click(function () {
+                                                alert("We got your order, total cost will be " + "Ksh." + finalCharges + " . Your pizza will be delivered to you in 20 minutes.")
+                                            });
                                         });
+
                                     });
-                                })
+                                });
                             });
                         });
                     });
@@ -76,3 +77,39 @@ $(document).ready(function () {
         });
     });
 });
+$(document).ready(function () {
+
+    $(".pizza-size").change(function (event) {
+        var total = 0;
+        $(".pizza-size").each(function () {
+            total += parseInt($(this).val())
+
+            $(".pizza-toppings").change(function (event) {
+                $(".pizza-toppings").each(function () {
+                    var toppingsTotal = total + parseInt($(this).val())
+
+                    $(".pizza-crust").change(function (event) {
+                        $(".pizza-crust").each(function () {
+                            var crustTotal = toppingsTotal + parseInt($(this).val())
+
+                            $(".form-quantity").change(function (event) {
+                                $(".form-quantity").each(function () {
+                                    var orderTotal = crustTotal * parseInt($(this).val())
+
+                                    $('input[type = "radio"]#no-deliver').click(function () {
+                                        $("#submit").click(function () {
+                                            alert("We got your order, total cost will be " + "Ksh." + orderTotal + " . Ready for pick-up in 15 min")
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+$(document).ready(function () {
+
+})
