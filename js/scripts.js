1ec692dc27f687b8ffaab24bbd6075d0df5f91b6
yvonnewangui <yvonne.wamburu@student.moringaschool.com>
2021-08-24 23:21:59 +0300

    a trial

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,30 @@ $(document).ready(function(){
     });
     $('input[type = "radio"]#yes-deliver').click(function(){
         $(".location").show();
-    })
+    });
 });
+
+
+
+//pizza order 
+var finalPriceArray = [];
+function Order (size, toppings, crust, quantity, delivery){
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.quantity = quantity;
+    this.delivery = delivery;
+    this.pizzaPrice = 0;
+}
+
+Order.prototype.singlePizzaCost = function(){
+    if(this.size ==="small"){
+        this.pizzaPrice += 300;
+    }
+    if(this.size === "medium"){
+        this.pizzaPrice += 450;
+    }
+    else if(this.size === "large"){
+        this.pizzaPrice += 650;
+    }
+}
