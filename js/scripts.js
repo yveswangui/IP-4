da103d3e3f9b5fd5325b9f062d4e5a41d4cace50
yveswangui <yveswangui@gmail.com>
2022-03-30 19:06:20 +0300

    get pizza values, add prices

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,47 @@ $(document).ready(function () {
         $(".location").show();
     });
 });
+
+
+$(document).ready(function(){
+    $('#addpizza').click(function(event){
+        event.preventDefault()
+
+        var form = document.getElementById('order');
+        var vegan = document.getElementById('vegeterian');
+        if(vegan.checked === true){
+            console.log('Vegan Pizza');
+        } else {
+            //non-vegan
+        };
+        var nonVegan = document.getElementById('non-vegeterian');
+        if(nonVegan.checked === true){
+            console.log('Non-Vegan Pizza');
+        } else {
+            // vegan
+        };
+
+        var pizzaSize = $("#size").val();
+        var pizzaCrust = $("#crust").val();
+        var pizzaToppings = $("#toppings").val();
+        var newOrder = pizzaSize + pizzaCrust + pizzaToppings;
+        console.log(newOrder);
+
+
+        //PIZZA PRICES
+        if (pizzaSize === 'small'){
+            console.log(500);
+        } else if (pizzaSize === 'medium'){
+            console.log(750);
+        } else if (pizzaSize ==='large'){
+            console.log(900);
+        } else {
+            alert('Pizza Size required for full meal!')
+        }
+
+       
+        
+        $('#pizza-message').append("<tr>" + "<td>" + pizzaSize + "</td>" + "<td>" + pizzaCrust + "</td>" + " " + "<td>" + pizzaToppings +"</td>" + "</tr>")
+        form.reset();
+    });
+});
